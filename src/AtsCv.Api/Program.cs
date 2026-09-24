3f2e8693c7e4f5f292a05ea6bd0fa8d871fcf163
Ürem Sancaktutan <36550960+codelovingcat@users.noreply.github.com>
2026-09-25 00:31:22 +0300

    feat: add baseline request rate limiting

--- src/AtsCv.Api/Program.cs
+++ src/AtsCv.Api/Program.cs
@@ -3,7 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 if (app.Environment.IsDevelopment())
 {
